768ebeb3af2c5f85f93cbe72163a8fe376695159
Dimas Putra <dimasputranugroho25@gmail.com>
2025-03-27 01:50:11 +0700

    Menambahkan fitur CRUD Furniture simple

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\FurnitureController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::resource('furniture', FurnitureController::class);
 
 Route::get('/home', function () {
     return view('home');
